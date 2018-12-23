2b7052284ae727dfa84e3b5e172b2dc3fadeb337
xpetrus <phsiao24@gmail.com>
2018-12-22 21:13:14 -0500

    functionality

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ var config = {
   var tname = "";
   var destination = "";
   var ftt = "";
-  var frequency = "";
+  var frequency = 0;
+
+  var minTillTrain = 0;
+  var nextArrival = "";
+  
+
+  console.log("Current Time: "+ moment().format("hh:mm"));
 
 $("#submit").on("click", function(event){
     event.preventDefault();
@@ -27,14 +33,35 @@ $("#submit").on("click", function(event){
     ftt = $("#first-train-time").val().trim();
     frequency = $("#frequency").val().trim();
 
+    var ftconverted = moment(ftt, "HH:mm").subtract(1, "years");
+   
+
+    var diffTime = moment().diff(moment(ftconverted), "minutes");
+    //console.log("difference in minutes: "+diffTime);
+
+    var tRemain = diffTime % frequency;
+    //console.log(tRemain);
+
+    minTillTrain = frequency - tRemain;
+    console.log("Min till trains: "+minTillTrain);
+    nextArrival = moment().add(minTillTrain, "minutes");
+    var nextArrivalTime = moment(nextArrival).format("hh:mm");
+    console.log("Next Arrival" + nextArrivalTime);
+
+
     database.ref().push({
         tname:tname,
         destination:destination,
         ftt:ftt,
-        frequency:frequency
+        frequency:frequency,
+        nextArrival:nextArrivalTime,
+        minTillTrain:minTillTrain
     });
 
-    
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
 
     //console.log(trains);
 
@@ -43,6 +70,23 @@ $("#submit").on("click", function(event){
 
 database.ref().on("child_added", function(snap){
     console.log(snap.val().tname);
+    console.log(snap.val().destination);
+    console.log(snap.val().ftt);
+    console.log(snap.val().frequency);
+
+    var nrow = $("<tr>");
+    nrow.append("<td>"+snap.val().tname+"</td>"
+    +"<td>"+snap.val().destination+"</td>"
+    +"<td>"+snap.val().frequency+"</td>"
+    +"<td>"+snap.val().nextArrival+"</td>"
+    +"<td>"+snap.val().minTillTrain+"</td>");
+    
+    
+    $("#schedule").append(nrow);
+
+
+    
+
 })
 
 
